7b1033edbd8b7421e3ed36c05718acff5d2f3a98
Alexandr Devyatkin <novealx@gmail.com>
2019-09-23 21:57:33 +0300

    fix main.py and docker-compose

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import asyncio
 import blog_repository
 import ast
 import validation
+import json
 
 routes = web.RouteTableDef()
 
@@ -12,54 +13,48 @@ routes = web.RouteTableDef()
 async def get_posts(request):
     pool = request.app['pool']
     async with pool.acquire() as connection:
-        async with connection.transaction():
-            result = await blog_repository.get_posts(connection)
-            return web.Response(text=str(result))
+        result = await blog_repository.get_posts(connection)
+        return web.json_response(result)
 
 
 @routes.get(r'/posts/{id}')
 async def get_post_by_id(request):
     pool = request.app['pool']
     async with pool.acquire() as connection:
-        async with connection.transaction():
-            result = await blog_repository.get_post_by_id(connection, int(request.match_info['id']))
-            if result is not None:
-                return web.Response(text=str(result))
-            else:
-                return web.Response(status=400, text='Некорректные данные')
+        result = await blog_repository.get_post_by_id(connection, int(request.match_info['id']))
+        if result is not None:
+            return web.json_response(result)
+        else:
+            return web.Response(status=400, text='Некорректные данные')
 
 
 @routes.post(r'/posts')
 async def add_post(request):
-
     content = await request.content.read()
     if not await validation.validate_post_fields(str(content, 'UTF-8')):
         return web.Response(status=400, text='Некорректные данные')
 
     pool = request.app['pool']
     async with pool.acquire() as connection:
-        async with connection.transaction():
-            request_body = ast.literal_eval(str(content, 'UTF-8'))
-            author_id = await blog_repository.get_author_id_by_name(connection, request_body['author'])
-            post_json = await blog_repository.add_post(connection, request_body, author_id)
-            return web.Response(status=201, text=str(post_json))
+        request_body = json.loads(content)
+        author_id = await blog_repository.get_author_id_by_name(connection, request_body['author'])
+        post_json = await blog_repository.add_post(connection, request_body, author_id)
+        return web.json_response(post_json, status=201)
 
 
 @routes.put(r'/posts/{id}')
 async def update_post(request):
-
     content = await request.content.read()
     if not await validation.validate_post_fields(str(content, 'UTF-8')):
         return web.Response(status=400, text='Некорректные данные')
 
     pool = request.app['pool']
     async with pool.acquire() as connection:
-        async with connection.transaction():
-            request_body = ast.literal_eval(str(content, 'UTF-8'))
-            request_body['id'] = request.match_info['id']
-            author_id = await blog_repository.get_author_id_by_name(connection, request_body['author'])
-            result = await blog_repository.update_post(connection, request_body, author_id)
-            return web.Response(status=200, text=str(result))
+        request_body = json.loads(content)
+        request_body['id'] = request.match_info['id']
+        author_id = await blog_repository.get_author_id_by_name(connection, request_body['author'])
+        result = await blog_repository.update_post(connection, request_body, author_id)
+        return web.json_response(result, status=200)
 
 
 async def init_app():
@@ -69,6 +64,7 @@ async def init_app():
     return app
 
 
-loop = asyncio.get_event_loop()
-application = loop.run_until_complete(init_app())
-web.run_app(application)
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    application = loop.run_until_complete(init_app())
+    web.run_app(application)
